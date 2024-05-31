0cb9bc4f089ac4467a79ee088217799a864aab5d
Singsheesh <tempdumacc4@gmail.com>
2024-05-30 23:28:26 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Information Gather</title>
+</head>
+<body>
+    <h1>Please Enter Your Information</h1>
+
+    <form action="info.php" method="POST"><br>
+    <label for="firstname">Enter your First name:  </label><br>
+    <input type="text" name="firstname"><br>
+    <label for="middlename">Enter your Middle name:  </label><br>
+    <input type="text" name="middlename"><br>
+    <label for=":Lastname">Enter your Last name:  </label><br>
+    <input type="text" name="lastname"><br>
+    <label for="age">Enter your Age:  </label><br>
+    <input type="number" name="age"><br>
+    <label for="contact">Enter your Contact no.#:  </label><br>
+    <input type="number" name="contact"><br>
+    <label for="address">Enter your Address:  </label><br>
+    <input type="text" name="address"><br>
+    <label for="Course">Enter your Course:  </label><br>
+    <input type="text" name="course"><br>
+    <input type="submit" name="Submit">
+
+    </form>
+</body>
+</html>
+
